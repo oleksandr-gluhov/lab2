6bb9044be51259a62f6506e3db78e3b70197a610
sasha <oleksandrgluhov6@gmail.com>
2022-10-06 22:13:29 +0300

    branch_02

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,16 +1,23 @@
+// Lab_02.cpp
+// < прізвище, ім’я автора >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 3
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-	double x; // вхідний параметр
+	double a; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	// double z2; // результат обчислення 2-го виразу
-	cout << "x = "; cin >> x;
-	z1 = 2 * x;
-	// z2 = x + x;
+	double z2; // результат обчислення 2-го виразу
+	cout << "a = "; cin >> a;
+	z1 = ( sin(2*a) + sin(5*a) - sin(3*a) ) / (cos(a) + 1 - 2*(sin(2*a) * sin(2*a) ));
+	//z2 = sin(2*a);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
